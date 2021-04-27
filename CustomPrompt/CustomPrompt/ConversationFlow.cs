7337153c896951bca30d528b7f9126895bc8813e
shilpagill41@gmail.com <shilpagill41@gmail.com>
2021-04-27 16:09:30 +0530

    CustomPrompt

---
+++ CustomPrompt/CustomPrompt/ConversationFlow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomPrompt
+{
+    public class ConversationFlow
+    {
+        public enum Question { Name, Age, Date, None}
+        public Question LastQuestionAsked { get; set; } = Question.None;
+    }
+}
---
+++ CustomPrompt/CustomPrompt/UserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomPrompt
+{
+    public class UserData
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public string Date { get; set; }
+    }
+}
